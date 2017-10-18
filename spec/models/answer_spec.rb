c76a4ef27ebeefc3a1092643fb04a975509b20f2
Tyler <twr349@gmail.com>
2017-10-18 22:05:51 +0000

    assignment 14 merge error fix

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  let(:question) { Question.create!(title: "New Question Title", body: "New Question Body", resolved: true) }
+  let(:question) { Question.create!(title: "New Question Title", body: "New Question Body") }
   let(:answer) { Answer.create!(body: 'Answer Body', question: question) }
   
   describe "attributes" do
@@ -9,4 +9,6 @@ RSpec.describe Answer, type: :model do
           expect(answer).to have_attributes(body: "Answer Body")
         end
     end
+    
+    
 end
