0a08df54e95adb537dc9def4d79027af06d45392
Tyler <twr349@gmail.com>
2017-10-19 20:39:38 +0000

    Checkpoint and Assignment 16 completed

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Workspace
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+      config.autoload_paths << File.join(config.root, "lib")  
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
@@ -22,5 +23,6 @@ module Workspace
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     #config.active_record.raise_in_transactional_callbacks = true
+    
   end
 end
