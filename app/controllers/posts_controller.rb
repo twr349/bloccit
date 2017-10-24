bbe8ecf8aa6499ceedb79799cdad3a2dfba1777a
Tyler <twr349@gmail.com>
2017-10-24 16:55:21 +0000

    Assignment 17 completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,13 @@
 class PostsController < ApplicationController
+  
   def index
-    
     @posts = Post.all
+    
+    @posts.each_with_index do |post, index|
+      if index % 5 == 0
+        post.title = "SPAM"
+      end
+    end
   end
 
   def show
