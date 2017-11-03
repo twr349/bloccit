f6ad343acb180858c82d623a15abde0fb44ba59d
Tyler <twr349@gmail.com>
2017-11-03 17:05:26 +0000

    Assignment 24 completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,19 @@
 class UsersController < ApplicationController
+
+   skip_before_action :verify_authenticity_token
+   ActionController::Parameters.permit_all_parameters = true
+   params = ActionController::Parameters.new
+    
     def new
      @user = User.new
     end
     
+    def user_params
+     params.require(:user).permit(:email, :name, :password, :password_confirmation)
+    end
+    
     def create
-     @user = User.new
+     @user = User.new(user_params)
      @user.name = params[:user][:name]
      @user.email = params[:user][:email]
      @user.password = params[:user][:password]
@@ -18,5 +27,15 @@ class UsersController < ApplicationController
        render :new
      end
     end
+    
+    
+    
+    def confirm
+     @user = User.new
+     @user.name = params[:user][:name]
+     @user.email = params[:user][:email]
+     @user.password = params[:user][:password]
+     @user.password_confirmation = params[:user][:password_confirmation]
+    end    
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,8 @@ Rails.application.routes.draw do
 
   resources :advertisements
 
-
+  post 'users/confirm' => 'users#confirm'
+  
   get 'about' => 'welcome#about'
   
   get 'contact' => 'welcome#contact'
