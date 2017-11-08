ae5d4e93568e939efcf4e5b4e964205a70b2ea58
Tyler <twr349@gmail.com>
2017-11-08 21:41:51 +0000

    Bad branch do not use

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,11 @@
 class Post < ApplicationRecord
     belongs_to :topic
+    belongs_to :user, optional: true
     has_many :comments, dependent: :destroy
     
    validates :title, length: { minimum: 5 }, presence: true
    validates :body, length: { minimum: 20 }, presence: true
    validates :topic, presence: true
+   validates :user, presence: true
 
 end
