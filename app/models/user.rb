005b733f8670b287a9df5d90749c37dd5c57edd9
Tyler <twr349@gmail.com>
2017-11-09 21:44:04 +0000

    Checkpoint 28 completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     
     has_many :posts, dependent: :destroy
+    has_many :comments, dependent: :destroy
     
     before_save { self.email = email.downcase if email.present? }
     before_save :format_name
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
       
   end
   
+  resources :posts, only: [] do
+    resources :comments, only: [:create, :destroy]
+  end
+  
   resources :users, only: [:new, :create]
   
   resources :sessions, only: [:new, :create, :destroy]
