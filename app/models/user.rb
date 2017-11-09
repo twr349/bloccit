bec97ed7b2da6a340fe9fc6eaedf93e86b837a30
Tyler <twr349@gmail.com>
2017-11-09 16:49:12 +0000

    Half way thru checkpoint

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
     
     before_save { self.email = email.downcase if email.present? }
     before_save :format_name
+    before_save { self.role ||= :member }
     
     validates :name, length: { minimum: 1, maximum: 100 }, presence: true
     validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
@@ -14,6 +15,8 @@ class User < ApplicationRecord
              length: { minimum: 3, maximum: 254 }
 
     has_secure_password
+    
+    enum role: [:member, :admin]
     
     def format_name
         if name
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171108215020) do
+ActiveRecord::Schema.define(version: 20171109163854) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string "title"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 20171108215020) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role"
   end
 
 end
