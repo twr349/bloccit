1927113b66fe93e2489e5215cc9b4e7054b70f15
Tyler <twr349@gmail.com>
2017-11-13 19:54:25 +0000

    Half way through checkpoint 30

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
     has_many :posts, dependent: :destroy
     has_many :comments, dependent: :destroy
     has_many :votes, dependent: :destroy
+    has_many :favorites, dependent: :destroy
     
     before_save { self.email = email.downcase if email.present? }
     before_save :format_name
@@ -28,6 +29,10 @@ class User < ApplicationRecord
             end
         self.name = name_arr.join(" ")
         end
-    end    
+    end 
+    
+    def favorite_for(post)
+     favorites.where(post_id: post.id).first
+    end
 
 end
